3bcfcd795649753ea9ea4d6e0be38acd0c9b4889
林云富 <linyunfu@linyunfus-Mac-mini.local>
2018-09-04 00:11:30 +0800

    dataObserve

---
+++ mvvm/mvvm.js
@@ -0,0 +1,27 @@
+function Yf(options={}){
+        this.$options = option;
+        var data =this._data=this.$options.data
+        observe(data);
+}
+function Observe(data){
+    for(let key in data){
+        let val= data[key];
+        observe(val)
+        Object.defineProperty(data,key,{
+            enumerable:true,
+            get(){
+                return val;
+            },
+            set(newVal){
+                if(newVal===val){
+                    return;
+                }else{
+                    val = newVal
+                }
+            }
+        });
+    }
+}
+function observe(data){
+    return new observe(data)
+}
