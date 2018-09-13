6db5973c93b8baa2f289adbbcb1e1e973fac63c8
--list <lin13177896353@163.com>
2018-09-14 00:10:47 +0800

    promise test

--- promise.js
+++ promise.js
@@ -103,6 +103,13 @@ Promise.prototype.then = function (onfulfilled, onrejected) {
         }
     })
     return promise2
-
+}
+Promise.defer= Promise.deferred=function(){
+    let dfd={}
+    dfd.promise=new Promise((resolve,reject)=>{
+        dfd.resolve=resolve;
+        dfd.reject=reject;
+    })
+    return dfd;
 }
 module.exports = Promise
