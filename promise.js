caf528c6cf877af374151d2fd6e9646e9d6a9a7a
--list <lin13177896353@163.com>
2018-09-14 00:01:45 +0800

    wspromise

--- promise.js
+++ promise.js
@@ -27,20 +27,28 @@ function Promise(executor) {
     }
 
 }
-function resolvePromise(x, promise2, resolve, reject) {
+function resolvePromise(promise2, x, resolve, reject) {
     if (x === promise2) {
         return reject(new TypeError('xunhuan yinyong'))
     }
+    let called;// 用来防止多次调用
     if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
         // 处理所有的promise
         let then = x.then;
         if (typeof then === 'function') {
             then.call(x, (y) => {
-                resolve(y)
+                if (called) return;
+                called = true;
+                // 递归解析 如果resolve 是一个promise 就要不停的resolve的结果 进行处理
+                resolvePromise(promise2, y, resolve, reject)
             }, (e) => {
+                if (called) return;
+                called = true;
                 reject(e)
             })
         } else {
+            if (called) return;
+            called = true;
             resolve(x)
         }
 
@@ -51,26 +59,47 @@ function resolvePromise(x, promise2, resolve, reject) {
 Promise.prototype.then = function (onfulfilled, onrejected) {
     let self = this;
     console.log(self.status)
+    onfulfilled=typeof onfulfilled == 'function'?onfulfilled:val=>val;
+    onrejected=typeof onrejected == 'function'?onrejected:err=>{
+        throw err
+    };
     // 需要判断onfulfilled/onrejected的执行结果 和promise2的关系
 
     let promise2 = new Promise((resolve, reject) => {
         if (self.status === 'resolved') {
-            let x = onfulfilled(self.value);
-            resolvePromise(x, promise2, resolve, reject);
+            setTimeout(() => {
+                try {
+                    let x = onfulfilled(self.value);
+                    resolvePromise(promise2, x, resolve, reject);
+                } catch (error) {
+                    reject(error)
+                }
+            }, 0)
         }
         if (self.status === 'rejected') {
-            let x = onrejected(self.reason)
-            resolvePromise(x, promise2, resolve, reject);
+            setTimeout(() => {
+                let x = onrejected(self.reason)
+                resolvePromise(promise2, x, resolve, reject);
+            }, 0)
+
         }
         if (self.status === 'pending') {
-            self.onRejected.push(() => {
-                let x = onfulfilled(self.value)
-                resolvePromise(x, promise2, resolve, reject);
-            });
-            self.onRejected.push(() => {
-                let x = onrejected(self.value)
-                resolvePromise(x, promise2, resolve, reject);
-            })
+            setTimeout(() => {
+                self.onRejected.push(() => {
+                    let x = onfulfilled(self.value)
+                    resolvePromise(promise2, x, resolve, reject);
+                });
+            }, 0)
+            setTimeout(() => {
+                self.onRejected.push(() => {
+                    try {
+                        let x = onrejected(self.value)
+                        resolvePromise(promise2, x, resolve, reject);
+                    } catch (error) {
+                        reject(error)
+                    }
+                })
+            }, 0)
         }
     })
     return promise2
