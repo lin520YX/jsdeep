a48cc5ebe6f424b9fd2bc0c72d1047ddb2018f57
--list <lin13177896353@163.com>
2018-09-22 18:43:09 +0800

    node

--- 2.es6/15.decorator.js
+++ 2.es6/15.decorator.js
@@ -1,12 +1,22 @@
 // mobx react库
 // 装饰器模式 包装
 
-function coffee(){
+// function coffee(){
+
+// }
+// function sweetCoffee(){
+//     coffee()
+// }
+// function milkCoffee(){
+//     sweetCoffee()
+// }
+
+// 使用在哪里 类上使用包装类 包装类中的属性
+// 不能包装函数 @ es7
+
+@flag class Animal{
 
 }
-function sweetCoffee(){
-    coffee()
-}
-function milkCoffee(){
-    sweetCoffee()
+function flag(target){//指代 包装的那个人
+    target.flag='dongwu'
 }
